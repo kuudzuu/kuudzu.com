9ef92f9af2f61f451f8fa9aef73209e64b171574
kuudzu <kuudzu.on@gmail.com>
2026-02-04 23:12:18 -0500

    added godot video

--- js/art.js
+++ js/art.js
@@ -11,8 +11,15 @@ async function loadArt() {
   let innerHTML = "<br>";
 
   for (let i = 0; i < pictures.length; i++) {
+    let isvideo = (pictures[i].url.substr(pictures[i].url.length - 3) === "mp4")
+
     innerHTML += "<div><h2>" + pictures[i].title + "</h2></div>";
-    innerHTML += "<div><a href=\"" + pictures[i].url + "\" target=\"_blank\"><img src=\"" + pictures[i].url + "\" class=\"ArtPicture\"></a></div>";
+    if (isvideo) {
+      innerHTML += "<div><a href=\"" + pictures[i].url + "\" target=\"_blank\"><video src=\"" + pictures[i].url + "\" type=\"video/mp4\" autoplay=\"true\" loop=\"true\" class=\"ArtPicture\"></a></div>";
+    } else {
+      innerHTML += "<div><a href=\"" + pictures[i].url + "\" target=\"_blank\"><img src=\"" + pictures[i].url + "\" class=\"ArtPicture\"></a></div>";
+    }
+
     innerHTML += "<div class=\"Caption\"><p><i>" + pictures[i].desc + "</i></p></div>";
     innerHTML += (i === pictures.length-1 ? "" : "<br><hr><br>");
   }
